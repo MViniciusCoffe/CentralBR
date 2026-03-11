93764d1c5b2dc3faabe3c1c00f8899d1d1edd2a2
MViniciusCoffe <infobasicifrn2017marcusv@gmail.com>
2026-03-11 15:11:56 -0300

    feat: Add styles for selected tabs

--- components/SettingsPanel/sections/AboutSection.jsx
+++ components/SettingsPanel/sections/AboutSection.jsx
@@ -13,16 +13,16 @@ export default function AboutSection() {
     <>
       <div className={styles.lateralMenu}>
         <ul>
-          <li onClick={() => setTab("project")}>
+          <li onClick={() => setTab("project")} className={tab === "project" ? styles.selected : ""}>
             <p>Projeto</p>
           </li>
-          <li onClick={() => setTab("author")}>
+          <li onClick={() => setTab("author")} className={tab === "author" ? styles.selected : ""}>
             <p>Autor</p>
           </li>
-          <li onClick={() => setTab("controls")}>
+          <li onClick={() => setTab("controls")} className={tab === "controls" ? styles.selected : ""}>
             <p>Controles</p>
           </li>
-          <li onClick={() => setTab("sources")}>
+          <li onClick={() => setTab("sources")} className={tab === "sources" ? styles.selected : ""}>
             <p>Fontes</p>
           </li>
         </ul>
--- components/SettingsPanel/sections/ViewSection.jsx
+++ components/SettingsPanel/sections/ViewSection.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import styles from "./Sections.module.css";
 
+import MapTab from "./viewTabs/MapTab";
+import StyleTab from "./viewTabs/StyleTab";
+
 export default function ViewSection() {
   const [tab, setTab] = useState("map");
 
@@ -8,17 +11,17 @@ export default function ViewSection() {
     <>
       <div className={styles.lateralMenu}>
         <ul>
-          <li onClick={() => setTab("map")}>
+          <li onClick={() => setTab("map")} className={tab === "map" ? styles.selected : ""}>
             <p>Mapa</p>
           </li>
-          <li onClick={() => setTab("style")}>
+          <li onClick={() => setTab("style")} className={tab === "style" ? styles.selected : ""}>
             <p>Estilo</p>
           </li>
         </ul>
       </div>
       <div className={styles.menuContent}>
-        {tab === "map"}
-        {tab === "style"}
+        {tab === "map" && <MapTab />}
+        {tab === "style" && <StyleTab />}
       </div>
     </>
   );
